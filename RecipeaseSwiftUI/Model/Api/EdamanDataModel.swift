aa45124fb059e5a5c99679741c78e42be6587208
Mickael Ruzel <mickael.ruzel@gmail.com>
2021-12-08 18:26:00 +0100

    Test: Add fake data for preview and debug

--- RecipeaseSwiftUI/Model/Api/EdamanDataModel.swift
+++ RecipeaseSwiftUI/Model/Api/EdamanDataModel.swift
@@ -8,61 +8,67 @@
 import Foundation
 
 struct EdamamDataModel: Codable {
-    let hits: [Hit]
-}
-
-// MARK: - Hit
-struct Hit: Codable {
-    let recipe: Recipe
-}
-
-// MARK: - Recipe
-struct Recipe: Codable {
-    let uri: String
-    let label: String
-    let image: String
-    let source: String
-    let url: String
-    let shareAs: String
-    let yield: Int
-    let healthLabels: [String]
-    let ingredientLines: [String]
-    let ingredients: [Ingredient]
-    let calories, totalWeight: Double
-    let totalTime: Int
-}
-
-enum Unit: String, Codable {
-    case empty = "%"
-    case g = "g"
-    case kcal = "kcal"
-    case mg = "mg"
-    case µg = "µg"
-}
-
-// MARK: - Ingredient
-struct Ingredient: Codable {
-    let text: String
-    let quantity: Double
-    let measure: String?
-    let food: String
-    let weight: Double
-    let foodCategory: String?
-    let foodID: String
-    let image: String?
+    let recipes: [Recipe]
     
     enum CodingKeys: String, CodingKey {
-        case text, quantity, measure, food, weight, foodCategory
-        case foodID = "foodId"
-        case image
+        case recipes = "hits"
+    }
+    
+    // MARK: - Hit
+    struct Recipe: Codable {
+        let detail: Detail
+        
+        enum CodingKeys: String, CodingKey {
+            case detail = "recipe"
+        }
+        
+        // MARK: - Recipe
+        struct Detail: Codable {
+            let uri: String
+            let label: String
+            let image: String
+            let source: String
+            let url: String
+            let shareAs: String
+            let yield: Int
+            let healthLabels: [String]
+            let ingredientLines: [String]
+            let ingredients: [Ingredient]
+            let calories, totalWeight: Double
+            let totalTime: Int
+            
+            // MARK: - Ingredient
+            struct Ingredient: Codable {
+                let text: String
+                let quantity: Double
+                let measure: String?
+                let food: String
+                let weight: Double
+                let foodCategory: String?
+                let foodID: String
+                let image: String?
+                
+                enum CodingKeys: String, CodingKey {
+                    case text, quantity, measure, food, weight, foodCategory
+                    case foodID = "foodId"
+                    case image
+                }
+            }
+        }
     }
 }
 
-// MARK: - Total
-struct Total: Codable {
-    let label: String
-    let quantity: Double
-    let unit: Unit
+#if DEBUG
+extension EdamamDataModel {
+    static var sampleData: EdamamDataModel {
+        let bundle = Bundle.main
+        let url = bundle.url(forResource: "EdamamData", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        return try! JSONDecoder().decode(EdamamDataModel.self, from: data)
+    }
 }
+#endif
+
+
 
 
